970f7b271e07ccbea290a9ebf34b996ab39f2fd4
Christian Johansen <christian@cjohansen.no>
2012-09-17 10:46:08 +0200

    Working popen and DeferrableChildProcess

---
+++ lib/em_pessimistic/deferrable_child_process.rb
@@ -0,0 +1,59 @@
+# encoding: utf-8
+# --
+# The MIT License (MIT)
+#
+# Copyright (C) 2012 Gitorious AS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#++
+require "em_pessimistic/popen3"
+require "eventmachine"
+
+module EMPessimistic
+  class DeferrableChildProcess < EventMachine::Connection
+    include EventMachine::Deferrable
+
+    def initialize
+      super
+      @stdout = []
+      @stderr = []
+    end
+
+    def self.open(cmd)
+      EMPessimistic.popen3(cmd, DeferrableChildProcess)
+    end
+
+    def receive_data(data)
+      @stdout << data
+    end
+
+    def receive_stderr(data)
+      @stderr << data
+    end
+
+    def unbind
+      status = get_status
+      if status.exitstatus != 0
+        fail(@stderr.join, status)
+      else
+        succeed(@stdout.join, status)
+      end
+    end
+  end
+end
---
+++ lib/em_pessimistic/popen3.rb
@@ -0,0 +1,50 @@
+# encoding: utf-8
+# --
+# The MIT License (MIT)
+#
+# Copyright (C) 2012 Gitorious AS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#++
+require "eventmachine"
+
+# Original implementation at
+# https://gist.github.com/535644/
+#
+module EMPessimistic
+  class Popen3StderrHandler < EventMachine::Connection
+    def initialize(connection)
+      @connection = connection
+    end
+
+    def receive_data(data)
+      @connection.receive_stderr(data)
+    end
+  end
+
+  def self.popen3(*args)
+    new_stderr = $stderr.dup
+    rd, wr = IO::pipe
+    $stderr.reopen(wr)
+    connection = EM.popen(*args)
+    $stderr.reopen(new_stderr)
+    EM.attach(rd, Popen3StderrHandler, connection)
+    connection
+  end
+end
