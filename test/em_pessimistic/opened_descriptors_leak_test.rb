9162a8711ce72fbaaaea82b5c5e865bb3bf772af
Andrey Chergik <achergik@gmail.com>
2013-02-01 15:12:55 -0800

    Test REALLY added to check there is no leak for file descriptors.

---
+++ test/em_pessimistic/opened_descriptors_leak_test.rb
@@ -0,0 +1,78 @@
+# encoding: utf-8
+# --
+# The MIT License (MIT)
+#
+# Copyright (C) 2012 Gitorious AS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#++
+require "test_helper"
+require 'fosl/parser'
+require "em_pessimistic/deferrable_child_process"
+require 'awesome_print'
+
+describe EMPessimistic::DeferrableChildProcess do
+  include EM::MiniTest::Spec
+
+  def timeout_interval
+   5
+  end
+
+  it "has no dubious opened pipes and unix domain sockets after all child processes exited" do
+
+    parse = FOSL::Parser.new
+    pid = Process.pid
+    finished = 0
+
+    # Increase this amount to your ("system opened file descriptors limit" / 3) and you will meet "Errno::EMFILE: Too many open files" exception.
+    amount = 1
+
+    EM.add_periodic_timer(1) do
+
+      data = Hash[parse.lsof("+p #{pid}")[pid].files.group_by { |f| f[:type] }.select { |k,v| /^(?:unix|PIPE)$/.match(k) }]
+      if finished == amount
+        assert data["PIPE"], 'there are several PIPEs owned by this process'
+
+        # One Pipe is for "lsof" process. Two others for EM loop.
+        assert_equal 3, data["PIPE"].length
+        assert !data["unix"], 'all unix domain sockets for this process are closed'
+        done!
+      else
+        ap data
+      end
+
+    end
+
+    amount.times do |i|
+      process = EMPessimistic::DeferrableChildProcess.open("ruby -v")
+
+      process.callback do |stdout, stderr, status|
+        finished += 1
+      end
+
+      process.errback do |stderr, stdout, status|
+        finished += 1
+      end
+    end
+
+    wait!
+
+  end
+end
+
