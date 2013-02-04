dec00710e3183d2d129536085b7504ce6b2366f2
Andrey Chergik <achergik@gmail.com>
2013-02-04 08:31:16 -0800

    different default standard amount of file descriptors upon test start covered for any OS.

--- test/em_pessimistic/opened_descriptors_leak_test.rb
+++ test/em_pessimistic/opened_descriptors_leak_test.rb
@@ -26,8 +26,26 @@ require "test_helper"
 require 'fosl/parser'
 require "em_pessimistic/deferrable_child_process"
 
+
 describe EMPessimistic::DeferrableChildProcess do
   include EM::MiniTest::Spec
+
+  def setup
+    @parse = FOSL::Parser.new
+    @pid = Process.pid
+  end
+
+  def _lsof
+    Hash[ @parse.lsof("+p #{@pid}")[@pid].files.group_by { |f| f[:type] }.select { |k,v| /^(?:unix|PIPE|FIFO)$/.match(k) }]
+  end
+
+  def _pipes?(data)
+    data['PIPE'] || data['FIFO']
+  end
+
+  def _unix?(data)
+    data['unix']
+  end
 
   def timeout_interval
    5
@@ -35,23 +53,29 @@ describe EMPessimistic::DeferrableChildProcess do
 
   it "has no dubious opened pipes and unix domain sockets after all child processes exited" do
 
-    parse = FOSL::Parser.new
-    pid = Process.pid
     finished = 0
+    default_data = _lsof
+    default_pipes = _pipes?(default_data)
+    default_unix = _unix?(default_data)
 
     # Increase this amount to your ("system opened file descriptors limit" / 3) and you will meet "Errno::EMFILE: Too many open files" exception.
     amount = 1
 
     EM.add_periodic_timer(1) do
 
-      data = Hash[parse.lsof("+p #{pid}")[pid].files.group_by { |f| f[:type] }.select { |k,v| /^(?:unix|PIPE|FIFO)$/.match(k) }]
+      data = _lsof
+      pipes = _pipes?(data)
+      unix = _unix?(data)
       if finished == amount
-        pipe = data['PIPE'] || data['FIFO']
-        assert pipe, 'there are several PIPEs owned by this process'
+        assert pipes, 'there are several PIPEs owned by this process'
 
         # One Pipe is for "lsof" process. Two others for EM loop.
-        assert_equal 3, pipe.length
-        assert !data["unix"], 'all unix domain sockets for this process are closed'
+        assert_equal default_pipes.length, pipes.length
+        if default_unix
+          assert_equal default_unix.length, unix.length
+        else
+          assert !unix, 'all unix domain sockets for this process are closed'
+        end
         done!
       else
         ap data
